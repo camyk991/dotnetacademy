af7caa5ed664ee4c6544fe0e5b6ae1ccd4a5673b
camyk991 <kurczakpee@gmail.com>
2023-11-26 16:37:13 +0100

    Created Calculation class with calculation methods. Also worked on main function where messages are displayed and will be menu with calculation operations.

---
+++ Homework1/Calculation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Homework1
+{
+	public class Calculation
+	{
+		public double Operation(double aNumber, double bNumber, int operationIndex)
+		{
+
+
+			return 0;
+		}
+
+		public double Addition(double aNumber, double bNumber)
+		{
+			return aNumber + bNumber;
+		}
+
+		public double Substraction(double aNumber, double bNumber)
+		{
+			return aNumber - bNumber;
+		}
+
+		public double Multiplication(double aNumber, double bNumber)
+		{
+			return aNumber * bNumber;
+		}
+
+		public double Division(double aNumber, double bNumber)
+		{
+			if (bNumber == 0)
+				return 0;
+
+			return aNumber / bNumber;
+		}
+
+		public double Exponentiation(double aNumber, double bNumber)
+		{
+			return Math.Pow(aNumber, bNumber);
+		}
+
+		public double Factorial(double aNumber)
+		{
+			if (aNumber < 0)
+				return 0;
+
+			if (aNumber == 1 || aNumber == 0)
+				return 1;
+
+			return aNumber * Factorial(aNumber - 1);
+		}
+	}
+}
+
--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,7 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int operationIndex = 0 ;
+
+        while (operationIndex != 7)
+        {
+            bool isValidInput = false;
+
+            Console.WriteLine("Choose operation: ");
+            Console.Write("1. Addition | 2. Substraction | 3. Multiplication | 4. Division | 5. Exponentiation | 6. Factorial | 7. EXIT\n");
+
+            isValidInput = int.TryParse(Console.ReadLine(), out operationIndex);
+
+            if (isValidInput)
+            {
+                switch(operationIndex)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        Console.WriteLine("\nIt is not an option.");
+                        break;
+                }
+            } else
+            {
+                Console.WriteLine("\nWrong value. Try again.");
+            }
+
+            Console.WriteLine("");
+        }
     }
 }
-
