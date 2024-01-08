a8fa23879cfd798842cd56cf02a1c5ed86f624a3
camyk991 <kurczakpee@gmail.com>
2024-01-08 14:31:15 +0100

    Created simple class structure for Vehicle (base class), PassangerVehicle and CargoVehicle

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Homework2;
+﻿using System;
+using System.Collections.Generic;
 
-class Program
+// Base Vehicle class
+public abstract class Vehicle
 {
-    static void Main(string[] args)
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int YearOfManufacture { get; set; }
+    public string Color { get; set; }
+    public decimal Price { get; set; }
+    public string RegistrationNumber { get; set; }
+    public int Mileage { get; set; }
+
+
+    public abstract decimal CalculateRentalCost(int durationOfTrip, int travelDistance, int carCoefficient);
+
+
+}
+
+public class PassengerVehicle : Vehicle
+{
+    
+    public int LesseeRating { get; set; }
+
+    override public decimal CalculateRentalCost(int durationOfTrip, int travelDistance, int carCoefficient)
     {
-        Console.WriteLine("Hello, World!");
+
+
+        return 0;
     }
 }
 
+
+public class CargoVehicle : Vehicle
+{
+    public int CargoWeight { get; set; }
+
+
+    override public decimal CalculateRentalCost(int durationOfTrip, int travelDistance, int carCoefficient)
+    {
+
+
+        return 0;
+    }
+
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
