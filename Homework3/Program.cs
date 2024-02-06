33eed8f42e14cba7a9ba33f3f66f90725691126b
camyk991 <kurczakpee@gmail.com>
2024-02-06 13:51:21 +0100

    Added methods to get words and letters correct with requirements

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -15,16 +15,21 @@ class Program
         Task[] tasks = textFiles.Select(file => ProcessFile(file)).ToArray();
 
         await Task.WhenAll(tasks);
-
-        Console.WriteLine("Processing complete.");
     }
 
     static async Task ProcessFile(string filePath)
     {
         string content = await File.ReadAllTextAsync(filePath);
+        char[] wordSplitCharacters = { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?' };
 
-        string[] sentences = Regex.Split(content, @"(?<=[.!?])\s+");
-        string[] words = content.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] sentences = Regex.Split(content, @"(?<=[.!?])\s+")
+                                .Where(sentence => !string.IsNullOrWhiteSpace(sentence))
+                                .ToArray();
+
+        string[] words = content.Split(wordSplitCharacters)
+                                .Where(word => !string.IsNullOrWhiteSpace(word))
+                                .ToArray();
+
         int punctuationCount = content.Count(char.IsPunctuation);
 
         if (!Directory.Exists(@"/Users/inmos/Projects/dotNet/dotnetacademy/Homework3/output-books"))
@@ -34,7 +39,24 @@ class Program
 
 
         string longestSenteceByNumberOfCharacters = sentences.OrderBy(x => x.Length).Last();
-        //string shortestSentenceByNumberOfWords
+
+        string shortestSentenceByNumberOfWords = sentences.OrderBy(x => x.Split(wordSplitCharacters).Length).First();
+
+        string longestWord = words.OrderBy(x => x.Length).Last();
+
+        char mostCommonLetter = words
+            .SelectMany(word => word.ToLower())
+            .GroupBy(letter => letter)
+            .OrderBy(letters => letters.Count())
+            .Select(group => group.Key)
+            .Last();
+
+        string[] wordsSortedByTheNumberOfUsesInDescendingOrder =
+            words
+                .GroupBy(word => word.ToLower())
+                .OrderByDescending(word => word.Count())
+                .Select(group => group.Key)
+                .ToArray();
 
         Console.WriteLine($"Processed: {Path.GetFileName(filePath)}");
     }
